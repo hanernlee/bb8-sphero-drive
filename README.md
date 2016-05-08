49c1856f039afca33ef0b6b32b95377f5e2a28d3
Christopher Lee <hanernlee@gmail.com>
2016-05-08 17:44:04 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# BB-8 Sphero using Node.js
+***
+I was super lucky to receive BB-8 as a birthday gift and as soon as I learned that BB-8 Sphero was hackable, I dived straight into looking up just how to do it.
+
+After a few hours of googling, I narrowed down my search to these two resources, which I found to be awesomely useful.
+
+[Sphero.js](https://github.com/orbotix/sphero.js)
+[Cylon.js](https://cylonjs.com/documentation/drivers/bb8/)
+
+Being new to programming, it took me a good hour to get BB-8 connected but it was definitely worth it.
+
+![alt text](http://imgur.com/cvQYSTo)
+
+For now, BB-8 behaves like a RC Car, but hey at least I get to randomise it's colours and move it around using my laptop.
+***
---
+++ bb8.js
@@ -0,0 +1,63 @@
+"use strict"
+
+var Cylon = require('cylon');
+var keypress = require("keypress");
+
+
+    Cylon.robot({
+      connections: {
+        bluetooth: { adaptor: 'central', uuid: '4e51973b007a40db9e45a207c39a8065', module: 'cylon-ble'}
+      },
+
+      devices: {
+        bb8: { driver: 'bb8', module: 'cylon-sphero-ble'}
+      },
+
+      work: function(my) {
+
+        function handle(ch, key) {
+
+          if (key.ctrl && key.name === "c") {
+              process.stdin.pause();
+              process.exit();
+          }
+
+          switch (key.name) {
+
+            case "c":
+              my.bb8.randomColor();
+            break;
+
+            case "w":
+              my.bb8.randomColor();
+              my.bb8.roll(100, 90);
+            break;
+
+            case "d":
+              my.bb8.randomColor();
+              my.bb8.roll(100, 180);
+            break;
+
+            case "s":
+              my.bb8.randomColor();
+              my.bb8.roll(100, 270);
+            break;
+
+            case "a":
+              my.bb8.randomColor();
+              my.bb8.roll(100, 0);
+            break;
+
+            case "space":
+            my.bb8.stop();
+              }
+          }
+
+      keypress(process.stdin);
+      process.stdin.on("keypress", handle);
+
+      process.stdin.setRawMode(true);
+      process.stdin.resume();
+
+    }
+    }).start();
